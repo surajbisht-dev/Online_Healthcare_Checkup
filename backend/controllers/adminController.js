1b8b3ef4c2598b0691b3bb3276871f3fdae69c36
https://github.com/surajbisht-dev <surajbisht1906@gmail.com>
2025-04-06 23:28:54 +0530

    bcrypt error

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
