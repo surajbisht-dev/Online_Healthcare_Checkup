8876a1d3d93225796055ba5c37bdbd90e6deac9f
https://github.com/surajbisht-dev <surajbisht1906@gmail.com>
2025-04-30 22:19:40 +0530

    date change

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,48 +1,41 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContext = createContext();
+
 const AppContextProvider = (props) => {
-  const currency = "$";
+  const [loading, setLoading] = useState(false);
+  const [currency, setCurrency] = useState("₹");
 
   const calculateAge = (dob) => {
+    const birthDate = new Date(dob);
     const today = new Date();
-    const birDate = new Date(dob);
-    // console.log(birDate);
-
-    let age = today.getFullYear() - birDate.getFullYear();
-    // console.log(age);
-
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
     return age;
   };
 
-  const months = [
-    "",
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-
-  const slotDateFormat = (slotDate) => {
-    const dateArray = slotDate.split("_");
-    return (
-      dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
-    );
+  const slotDateFormat = (dateStr) => {
+    const date = new Date(dateStr);
+    const offset = date.getTimezoneOffset(); // get timezone offset in minutes
+    date.setMinutes(date.getMinutes() - offset); // adjust for local timezone
+    return date.toISOString().split("T")[0]; // keep format 'YYYY-MM-DD'
   };
 
   const value = {
+    loading,
+    setLoading,
+    currency,
+    setCurrency,
     calculateAge,
     slotDateFormat,
-    currency,
   };
+
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
